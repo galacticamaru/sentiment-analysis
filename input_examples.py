d43d4a9263f699047cebd1674919071016e1d121
galacticamaru <50388383+galacticamaru@users.noreply.github.com>
2025-03-10 11:32:12 +1000

    Create input_examples.py

---
+++ input_examples.py
@@ -0,0 +1,8 @@
+# a representation of the default values of inputs in the Hex notebook
+input_keywords = "result, email"
+
+input_start_date = "2023-02-28"
+
+input_end_date = "2025-02-28"
+
+runner = False
